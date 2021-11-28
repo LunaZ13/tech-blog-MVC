601de0b979797bf6be00b45450c04ce7e3c1fc00
inmarluna <inmarluna@Inmars-MacBook-Pro.local>
2021-11-28 13:56:16 -0600

    created user model & api routes

--- models/User.js
+++ models/User.js
@@ -8,10 +8,25 @@ class User extends Model {}
 User.init(
   {
     // TABLE COLUMN DEFINITIONS GO HERE
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    username: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            len: [8]
+        }
+    }
   },
   {
-    // TABLE CONFIGURATION OPTIONS GO HERE (https://sequelize.org/v5/manual/models-definition.html#configuration))
-
     // pass in our imported sequelize connection (the direct connection to our database)
     sequelize,
     timestamps: false,
